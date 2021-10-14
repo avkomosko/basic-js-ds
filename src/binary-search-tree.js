be79429bd009c40379c891a2eb27b3adc673f690
Aliaksei <avkomosko@gmail.com>
2021-10-14 20:52:36 +0300

    feat implement all functions in BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -63,7 +63,7 @@ module.exports = class BinarySearchTree {
     } else {
       return findNode(this._root, data);
     }
-    
+
     function findNode(node, data) {
       if (node.data === data) {
         return node;
@@ -74,15 +74,71 @@ module.exports = class BinarySearchTree {
   }
 
   remove(data) {
-   
+    if (!this.has(data)) {
+      return null;
+    } else {
+      this._root = removeNode(this._root, data);
+    }
+    function removeNode(node, data) {
+      if (!node) {
+        return null;
+      }
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if (node.data < data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+        let minRight = node.right;
+        while (minRight.left) {
+          minRight = minRight.left;
+        }
+        node.data = minRight.data;
+        node.right = removeNode(node.right, minRight.data);
+        return node;
+      }
+    } 
   }
 
   min() {
-    
+     if (!this._root) {
+      return;
+    }
+
+    let node = this._root;
+    while (node.left) {
+      node = node.left;
+    }
+
+    return node.data;
   }
 
   max() {
-    
+     if (!this._root) {
+      return;
+    }
+
+    let node = this._root;
+    while (node.right) {
+      node = node.right;
+    }
+
+    return node.data;
   }
 
 }
